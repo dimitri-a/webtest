92ed96ebc90bb14003aa3a99b065748e3fb9f8ca
dimitric <couwenberg@gmail.com>
2017-11-13 23:39:38 +1100

    created table for players

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 /* PUT YOUR CODE HERE */
+$(document).ready(function () {
+    var items = [];
+    $.getJSON("data/players.json", function (data) {
+        for (var i = 0; i < data.players.length; i++) {
+            // console.log(data.players[i].full_name);
+            items.push(
+                {
+                    player: data.players[i].full_name,
+                    matches: data.players[i].matches,
+                    tries: data.players[i].tries
+                }
+            );
+        }
+    });
 
-console.log('hello');
+    var tbl = $("<table/>").attr("id", "mytable");
+    $(".json-module .module-content").append(tbl);
+    for (var i = 0; i < items.length; i++) {
+        var tr = "<tr>";
+        var td1 = "<td>" + items[i]["player"] + "</td>";
+        var td2 = "<td>" + items[i]["matches"] + "</td>";
+        var td3 = "<td>" + items[i]["tries"] + "</td></tr>";
+
+        $("#mytable").append(tr + td1 + td2 + td3);
+    }
+    console.log('hello');
+})
+;
