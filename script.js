fa0c565fce190be59cad0fc1c109c0fbb5764902
dimitric <couwenberg@gmail.com>
2017-11-14 07:52:13 +1100

    clean

--- script.js
+++ script.js
@@ -3,32 +3,20 @@ $(document).ready(function () {
     var players, stats, items = [];
     $.getJSON("data/players.json", function (data) {
         $.each(data, function (key, players) {
-            // console.log('players=', players);
             $.getJSON("data/stats.json", function (data) {
                 $.each(data, function (key, stats) {
-                    console.log('stats=', stats);
-                    console.log('players=', players);
-
                     //check stats available for players:
                     for (var i = 0; i < players.length; i++) {
                         for (var j = 0; j < stats.length; j++) {
-
                             var exists = items.some(function (item) {
                                 return (item.player === players[i].full_name)
                             })
-                            console.log('exists=', exists);
                             if (players[i].id === stats[j].id && !exists) {
-                                console.log('bingo');
                                 items.push({
                                     player: players[i].full_name,
                                     matches: stats[j].matches,
                                     tries: stats[j].tries,
-
                                 })
-                                console.log('items=', items);
-
-
-
                             }
                         }
                     }
